34fef47e7186a7ac6ca9888c18ad6a63cef1e120
Minghao Zhu <minghaoz@andrew.cmu.edu>
2024-08-19 00:34:02 -0700

    TP-3 trip details component

--- trip-planner/src/app/app-routing.module.ts
+++ trip-planner/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { TripDetailsComponent } from './trip-details/trip-details.component';
+const routes: Routes = [
+  { path: 'trip-details', component: TripDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
